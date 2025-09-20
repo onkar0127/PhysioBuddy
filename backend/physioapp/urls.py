e796b718c376cb0721b937b20cb75e21abea5380
Sailesh-Bhoite <saileshbht@gmail.com>
2025-09-20 11:48:31 +0530

    Deleted unnecessary urls

--- backend/physioapp/urls.py
+++ backend/physioapp/urls.py
@@ -4,15 +4,18 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('video-feed/', views.video_feed, name='video_feed'),
-    path('', views.webcam_page, name='webcam_page'),
-    path('api/test/', views.test_api),
+#    path('video-feed/', views.video_feed, name='video_feed'),
+#    path('', views.webcam_page, name='webcam_page'),
+#    path('api/test/', views.test_api),
 
     path('login/', views.login_view),
-    path('doctor-profile/', views.doctor_profile),
-    path('patient-profile/', views.patient_profile),
 
     # API endpoints
+    path('api/login/', views.login_api),
     path('api/patient/profile/', views.patient_profile_api),
     path('api/doctor/profile/', views.doctor_profile_api),
+
+    # Tkinter URL
+    path('tk/', views.tk),
+    path('start-exercise/', views.start_exercise),
 ]
