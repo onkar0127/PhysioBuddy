97b6bed88a27a9c116ac27afdd99feb1f9f85884
Kalpesh Krishna Gawas <gawaskalpesh144@gmail.com>
2025-10-07 11:50:41 +0530

    checking

--- frontend/src/pages/AssignExercise.jsx
+++ frontend/src/pages/AssignExercise.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./AssignExercise.css";
 import { useNavigate } from "react-router-dom";
-
+//hhjkklkl
 const AssignExercise = () => {
   const navigate = useNavigate();
 
