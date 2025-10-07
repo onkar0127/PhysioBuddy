c4e4b9aeb285074fbb446e37541084f9b4ecd8a2
Sailesh-Bhoite <saileshbht@gmail.com>
2025-10-07 13:11:19 +0530

    Add WebStream route to App component for live video streaming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import D_Profile from './pages/D_Profile.jsx';
 import AssignExercise from './pages/AssignExercise.jsx';
+import WebStream from './pages/Webstream.jsx';
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/Profile" element={<D_Profile />} />
         <Route path="/assign-exercise" element={<AssignExercise />} />
-        
+        <Route path='/live' element={<WebStream />}/>
 
 
 
