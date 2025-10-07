d46f306c7d4f4669799bda2742f5c684975a85e3
Sailesh-Bhoite <saileshbht@gmail.com>
2025-10-07 12:43:43 +0530

    Implement ASGI configuration and add ExerciseConsumer for WebSocket handling

--- backend/physioapp/asgi.py
+++ backend/physioapp/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import physioapp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'physioapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "physioapp.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(physioapp.routing.websocket_urlpatterns)
+    ),
+})
