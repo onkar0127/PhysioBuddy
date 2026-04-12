040aac92364dccfd8389cb8795ef4a1ddff6abea
Sailesh-Bhoite <saileshbht@gmail.com>
2026-04-12 12:22:04 +0530

    Created API for patient status page

--- backend/physioapp/urls.py
+++ backend/physioapp/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('api/get-exercise-list/', views.get_exercise_list),
     path('api/patient/update-image/', views.update_patient_image),
     path('api/doctor/update-image/', views.update_doctor_image),
+    path('api/patient-status/', views.get_patient_status),
 ]
 
