af51d47264017689d80e541caa96c8c8c552a668
vedant-kawale-27 <vedantkawale0304@gmail.com>
2026-07-15 12:02:14 +0530

    fix: enable ASGI support with Daphne and update database seeding

--- backend/physioapp/settings.py
+++ backend/physioapp/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(','
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
